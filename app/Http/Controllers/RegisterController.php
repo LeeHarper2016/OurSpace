06df2c5086753b3caba0e5acbd964cc37378ca9d
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-12 16:14:52 -0600

    Update RegisterController.php
    Updated: RegisterController.php
    -> Created a function to display a view to the user.

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    //
+    /*************************************************************
+     *
+     * Function: RegisterController.view().
+     * Purpose: Displays a view containing a registration form to
+     *          to the user.
+     * Precondition: There is a valid register.blade.php file to
+     *               display.
+     * Postcondition: N/A.
+     *
+     * @returns view View containing registration form.
+     *
+     ************************************************************/
+    public function view() {
+        return view('auth.register');
+    }
 }
