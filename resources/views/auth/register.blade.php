c93d26708e746b66da39669947dc0950fe305a4e
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-12 03:30:13 -0600

    Create register.blade.php
    Created: register.blade.php
    -> This will be the page that a new user lands on to register an account.
    -> The design is an initial draft, and is subject to change.

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+@section('content')
+<div class="flex flex-col items-center mt-10">
+    <img width="200" height="200" />
+    <h1 class="text-4xl font-bold">Register</h1>
+    <form method="post" action="" class="flex flex-col mr-auto ml-auto space-y-5 mt-5">
+        @csrf
+        <div class="flex flex-col w-64">
+            <label for="name">Name:</label>
+            <input type="text" name="name" />
+        </div>
+        <div class="flex flex-col">
+            <label for="name">Email:</label>
+            <input type="email" name="email" />
+        </div>
+        <div class="flex flex-col">
+            <label for="name">Password:</label>
+            <input type="password" name="password" />
+        </div>
+        <div class="flex flex-col">
+            <label for="name">Confirm Password:</label>
+            <input type="password" name="passwordConfirm" />
+        </div>
+        <button type="submit" class="p-3 bg-gray-400 border border-black rounded w-20 ml-auto mr-auto">Submit</button>
+    </form>
+</div>
+@endsection
