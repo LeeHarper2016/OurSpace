2f310cff8a5c17820452cd8c858ed242d4d31e21
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-13 16:21:53 -0600

    Update RegisterController.php
    Updated: RegisterController.php
    -> Added a method that registers a new user provided that they enter valid information.

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -20,5 +22,39 @@ class RegisterController extends Controller
      ************************************************************/
     public function view() {
         return view('auth.register');
+    }
+
+    /*************************************************************
+     *
+     * Function Name: RegisterController.registerUser()
+     * Purpose: Registers a new user to the database.
+     * Precondition: N/A.
+     * Postcondition: A new user is added to the database.
+     *
+     * @param Request $request The total http request.
+     * @return RedirectResponse Redirection to the homepage.
+     *
+    *************************************************************/
+    public function registerUser(Request $request) {
+        if ($request->password !== $request->passwordCheck) {
+            return redirect('/register')
+                ->withErrors('The password combination entered was incorrect. Please try again');
+        } else {
+            $request->validate([
+                'name' => 'required',
+                'email' => 'required',
+                'password' => 'required'
+            ]);
+
+            auth()->user()
+                ->fill([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'password' => Hash::make($request->password),
+                ])
+                ->save();
+
+            return redirect('/')->with(['status' => 'Registration successful!']);
+        }
     }
 }
