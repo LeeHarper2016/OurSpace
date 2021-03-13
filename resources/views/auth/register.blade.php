9433d9dd30a4e6552a44734a90163420767e6334
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-13 16:42:20 -0600

    Update register.blade.php
    Updated: register.blade.php
    -> Added an include to display the summary of errors.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.app')
 @section('content')
+@include('errors.summary');
 <div class="flex flex-col items-center mt-10">
     <img width="200" height="200" />
     <h1 class="text-4xl font-bold">Register</h1>
-    <form method="post" action="{{ url('/register')  }}" class="flex flex-col mr-auto ml-auto space-y-5 mt-5">
+    <form method="post" action="{{ url('/users')  }}" class="flex flex-col mr-auto ml-auto space-y-5 mt-5">
         @csrf
         <div class="flex flex-col w-64">
             <label for="name">Name:</label>
