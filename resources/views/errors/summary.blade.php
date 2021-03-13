387f4e90dc199612787bf06a358c7de669e473f1
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-13 16:50:16 -0600

    Update summary.blade.php
    Updated: summary.blade.php
    -> Fixed the incorrect syntax for the error messages.

--- resources/views/errors/summary.blade.php
+++ resources/views/errors/summary.blade.php
@@ -1,8 +1,8 @@
 <div>
     @if(isset($errors))
-        @foreach($errors as $error)
+        @foreach($errors->all() as $error)
             <div class="bg-red-200 h-12 rounded-xl w-2/3 p-3 mr-auto ml-auto mt-3">
-                <span>{{ $error->message }}</span>
+                <span>{{ $error }}</span>
             </div>
         @endforeach
     @endif
