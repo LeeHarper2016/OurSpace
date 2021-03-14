d9073207afd8277c2c0bc6197c3380466a5a629e
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-14 16:17:22 -0500

    Update LoginController.php
    Updated: LoginController.php
    -> Added view() method, which will display a login form to the user when executed.

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+
+    /*****************************************************************************
+     *
+     * Function: LoginController.view().
+     * Purpose: Displays a view that contains a form that will allow the user
+     *          to log in to their account.
+     * Precondition: An auth.login view is established.
+     * Postcondition: N/A.
+     *
+     * @returns view View containing login form.
+     *
+     ****************************************************************************/
+    public function view() {
+        return view('auth.login');
+    }
 }
