d4f7a52854d16465b076c7629773fd461722fcd6
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-15 18:40:36 -0500

    Create 2021_03_15_020721_create_spaces_table.php
    Created: create_spaces_table.php
    -> Migration that will be used to create the "spaces" table.

---
+++ database/migrations/2021_03_15_020721_create_spaces_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSpacesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('spaces', function (Blueprint $table) {
+            $table->id();
+            $table->text('name');
+            $table->text('description');
+            $table->text('icon_picture_path');
+            $table->text('banner_picture_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('spaces');
+    }
+}
