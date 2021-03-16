088e5710149389856d555455819847c2d14b709c
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-15 21:05:54 -0500

    Update create.blade.php
    Updated: create.blade.php
    -> Fixed an error with the quotations of the enctype attribute.

--- resources/views/spaces/create.blade.php
+++ resources/views/spaces/create.blade.php
@@ -4,7 +4,8 @@
     <div class="flex flex-col items-center mt-10">
         <img width="200" height="200" />
         <h1 class="text-4xl font-bold">Create a Space!</h1>
-        <form method="post" action="{{ route('spaces.store')  }}" class="flex flex-col mr-auto ml-auto space-y-5 mt-5">
+        <form method="post" action="{{ route('spaces.store')  }}" enctype="multipart/form-data"
+              class="flex flex-col mr-auto ml-auto space-y-5 mt-5">
             @csrf
             <div class="flex flex-col">
                 <label for="name">Name:</label>
