434baeb158fc8b49abe2d9d6b2f39b0330f311cc
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-18 12:57:40 -0500

    Update User_In_Space.php
    Updated: User_in_Space.php
    -> Changed capitalization to be more standardized across the models.

--- app/Models/User_In_Space.php
+++ app/Models/User_In_Space.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class user_in_space extends Model
+class userInSpace extends Model
 {
     use HasFactory;
 }
