ec796040f8c8bbb170fce6de74b743155ad7a719
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-18 12:53:45 -0500

    Update Space.php
    Updated: Space.php
    -> Added the spaces() method to obtain a collection of users located within a space.

--- app/Models/Space.php
+++ app/Models/Space.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,4 +17,19 @@ class Space extends Model
         'icon_picture_path',
         'banner_picture_path'
     ];
+
+    /********************************************************************
+     *
+     * Function: User.spaces()
+     * Purpose: Retrieve a collection of all users that are in the space.
+     * Precondition: N/A.
+     * Posctondition: N/A.
+     *
+     * @return BelongsToMany The collection of users in the space.
+     *
+     *******************************************************************/
+    public function spaces() {
+        return $this->belongsToMany(User::class, 'user_in_spaces')
+            ->withTimestamps();
+    }
 }
