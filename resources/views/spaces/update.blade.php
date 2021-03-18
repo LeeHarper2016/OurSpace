159b996774785dbf403a8fe1bb260507e2698617
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-18 18:21:36 -0500

    Update update.blade.php
    Updated: update.blade.php
    -> Fixed an error within the route() helper call.

--- resources/views/spaces/update.blade.php
+++ resources/views/spaces/update.blade.php
@@ -4,7 +4,7 @@
     <div class="flex flex-col items-center mt-10">
         <img width="200" height="200" />
         <h1 class="text-4xl font-bold">Update {{ $space->name }}</h1>
-        <form method="post" action="{{ route('spaces.edit', ['space' => $space->id])  }}" enctype="multipart/form-data"
+        <form method="post" action="{{ route('spaces.update', ['space' => $space->id])  }}" enctype="multipart/form-data"
               class="flex flex-col mr-auto ml-auto space-y-5 mt-5">
             @csrf
             @method('PUT')
