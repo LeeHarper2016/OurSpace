5bd38c1d1a6ff3f64e9f848e66f6425c7c1fba28
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-17 19:18:20 -0500

    Update show_public.blade.php
    Updated: show_public.blade.php
    -> Improved the styling of the space detail cards.

--- resources/views/spaces/show_public.blade.php
+++ resources/views/spaces/show_public.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.app')
 @section('content')
 
-    <div class="flex flex-wrap space-x-5 space-y-5">
+    <div class="flex flex-wrap justify-center p-5">
         @foreach($spaces->all() as $space)
-        <div class="block min-w-10 min-h-20">
-            <img src="{{ asset($space['banner_picture_path'])  }}" />
-            <h1>{{ $space['name'] }}</h1>
-            <p>{{ $space['description'] }}</p>
+        <div class="block bg-indigo-300 w-52 h-52 m-5 border border-black rounded-md">
+            <img src="{{ asset($space['banner_picture_path']) }}" class="w-full h-20 rounded-t-md" />
+            <div class="p-5">
+                <h1 class="break-words text-xl font-bold">{{ $space['name'] }}</h1>
+                <p class="break-words">{{ $space['description'] }}</p>
+            </div>
         </div>
         @endforeach
     </div>
