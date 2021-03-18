405d783eae5628fe4af293c4de376caffcf689c3
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-18 13:00:52 -0500

    Update UserInSpace.php
    Updated: UserInSpace.php
    -> Removed underscores.

--- app/Models/UserInSpace.php
+++ app/Models/UserInSpace.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class userInSpace extends Model
+class UserInSpace extends Model
 {
     use HasFactory;
 }
