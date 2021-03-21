7907978fb852ff54bd45524f36811edcece9e6d6
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-20 22:25:46 -0500

    Update Particle.php
    Updated: Particle.php
    -> Implemented space() method to make a belongsTo() relationship between particles and the space it was posted to.

--- app/Models/Particle.php
+++ app/Models/Particle.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Particle extends Model
 {
     use HasFactory;
+
+
+
+    /********************************************************************
+     *
+     * Function: Particle.space()
+     * Purpose: Retrieve the space associated with the particle.
+     * Precondition: N/A.
+     * Posctondition: N/A.
+     *
+     * @return BelongsToMany The space that the particle was posted to.
+     *
+     *******************************************************************/
+    public function space() {
+        return $this->belongsTo(Space::class)
+            ->withTimestamps();
+    }
 }
