2a16a37334744e89ce54f76d3baf15a79b8e61ab
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-21 03:46:55 -0500

    Update ParticleController.php
    Updated: ParticleController.php
    -> Implemented a store() method that will store a user-supplied particle into the database.

--- app/Http/Controllers/ParticleController.php
+++ app/Http/Controllers/ParticleController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
+
+use App\Models\Particle;
 use Illuminate\Http\Request;
 
 class ParticleController extends Controller
 {
-    //
+    /**************************************************************************
+     *
+     * Function: ParticleController.store().
+     * Purpose: Stores a new particle to the database.
+     * Precondition: The data entered is valid and the user is authenticated.
+     * Postcondition: The particle is added to the database.
+     *
+     * @param int $spaceId The ID of the current space
+     * @param Request $request The whole http request.
+     * @return RedirectResponse Redirection back to the original page.
+     *
+     ************************************************************************/
+    public function store(int $spaceId, Request $request) {
+        $validatedData = $request->validate([
+            'body' => ['required', 'text']
+        ]);
+
+        $particle = new Particle;
+        $particle->fill([
+            'user_id' => auth()->user()->id,
+            'space_id' => $spaceId,
+            'body' => $validatedData['body']
+        ]);
+
+        $particle->save();
+
+        return back();
+    }
 }
