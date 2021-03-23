c88a3fa54f0d101f610995935f3c53f0379a7535
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-22 22:22:12 -0500

    Update UserController.php
    Updated: UserController.php
    -> Implemented update() method which will update the user's account information based on information provided by them.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Contracts\View\View;
 
@@ -26,6 +27,37 @@ class UserController extends Controller
             return view('user.edit')->with(['user' => $user]);
         } else {
             return back()->withErrors(['You are not currently logged in.w']);
+        }
+    }
+
+    /********************************************************************
+     *
+     * Function: UserController.update()
+     * Purpose: Updates the user's account using information given by the
+     *          user.
+     * Precondition: The user is currently authenticated.
+     * Posctondition: The user's account information is updated.
+     * TODO: Implement functionality for changing user icon.
+     *
+     * @param Request $request The whole http request.
+     * @return View Redirection back to the homepage.
+     *
+     ******************************************************************/
+    public function update(Request $request) {
+        $user = auth()->user();
+
+        if (isset($user)) {
+            $validatedDate = $request->validate([
+                'name' => 'required',
+                'email' => 'required'
+            ]);
+
+            $user->fill($validatedDate)
+                ->save();
+
+            return redirect('/')->with(['status' => 'Success']);
+        } else {
+            return redirect('/')->withErrors('You are not currently logged in.');
         }
     }
 }
