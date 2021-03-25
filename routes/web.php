b59e354525679e711be5c10887573fffd299eab4
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-25 17:41:37 -0500

    Update web.php
    Updated: web.php
    -> Added a route that allows the user to view a form to edit their account.
    -> Added a route that will apply the update information to the account.

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ParticleController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\SpaceController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,8 +27,13 @@ Route::get('/login', [LoginController::class, 'view']);
 Route::get('/logout', [LoginController::class, 'logOutUser']);
 
 Route::resource('spaces', SpaceController::class);
+
 Route::post('/spaces/{space}/particles', [ParticleController::class, 'store'])
     ->name('particles.store');
+
+Route::get('/account/edit', [UserController::class, 'edit']);
+Route::post('/account', [UserController::class, 'update'])
+    ->name('user.edit');
 
 Route::post('/users', [RegisterController::class, 'registerUser']);
 Route::post('/users/login', [LoginController::class, 'loginUser']);
