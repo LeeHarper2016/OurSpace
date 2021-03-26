db1705b19014d36b08d1889e10c78b03b950a022
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-25 19:13:39 -0500

    Delete summary.blade.php
    Deleted: summary.blade.php
    -> Removed error summary view in favor of a Vue component.

--- resources/views/errors/summary.blade.php
+++
@@ -1,9 +0,0 @@
-<div>
-    @if(isset($errors))
-        @foreach($errors->all() as $error)
-            <div class="bg-red-200 h-12 rounded-xl w-2/3 p-3 mr-auto ml-auto mt-3">
-                <span>{{ $error }}</span>
-            </div>
-        @endforeach
-    @endif
-</div>
