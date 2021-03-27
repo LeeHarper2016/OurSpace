e77ce97c38c315d521f0ba714164e875552d58d9
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-27 07:24:46 -0500

    Create ErrorList.vue
    Created: ErrorList.vue
    -> This component will display a list of error-message components.

---
+++ resources/js/components/error/ErrorList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="relative pointer-events-none w-full">
+        <error-message v-for="error in this.errors" v-bind:key="error.id">
+            {{ error }}
+        </error-message>
+    </div>
+</template>
+
+<script>
+import ErrorMessage from "./ErrorMessage";
+
+
+export default {
+    props: ['errors'],
+    components: {ErrorMessage},
+    data() {
+        return {
+            message: 'There was an error.'
+        }
+    }
+}
+</script>
