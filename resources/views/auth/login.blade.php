6e1dca79dcf224fd79a277033b9e06cc7926220f
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-27 06:23:41 -0500

    Update login.blade.php
    Updated: login.blade.php
    -> Removed the inclusion of the errors.summary view. This will eventually be replaced by an "error-list" Vue component within app.layout.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.app')
 @section('content')
-@include('errors.summary');
 <div class="flex flex-col items-center mt-10">
     <img width="200" height="200" />
     <h1 class="text-4xl font-bold">Login</h1>
