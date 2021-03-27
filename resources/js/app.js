1fef6235e2789bba6001397e6e167b0713b815c3
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-27 07:25:26 -0500

    Update app.js
    Updated: app.js
    -> Added Vue functionality.
    -> Implemented the ErrorList Vue component.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+import ErrorList from "./components/error/ErrorList";
+
+new Vue({
+    el: '#app',
+    components: {
+        ErrorList
+    }
+})
