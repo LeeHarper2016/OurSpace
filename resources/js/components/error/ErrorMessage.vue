ec4a445d22d9f55bb8f79fd2fc3f07283392a221
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-27 07:21:26 -0500

    Create ErrorMessage.vue
    Created: ErrorMessage.vue
    -> Created a Vue component for displaying a single error message.
    -> As opposed to the previous blade iteration, this has user interaction capabilities.
    -> This also fixed an underlying issue where a user could not click behind the container divs.

---
+++ resources/js/components/error/ErrorMessage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div v-if="isVisible" class="bg-red-200 h-12 rounded-xl w-2/3 p-3 mr-auto ml-auto mt-3 pointer-events-auto">
+        <slot></slot>
+        <button @click="toggleVisibility" class="underline">
+            close
+        </button>
+    </div>
+</template>
+
+<script>
+  export default {
+      data() {
+          return {
+              isVisible: true
+          }
+      },
+      methods: {
+          toggleVisibility() {
+              this.isVisible = false;
+          }
+      }
+
+  }
+</script>
