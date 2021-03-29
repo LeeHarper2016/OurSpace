81f599ea999fcfe0217b419a7be693d889cd6cfa
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-29 14:03:50 -0500

    Create ParticleForm.vue
    CreatedL ParticleForm.vue
    -> This component will be responsible for displaying a form to the user. The form will allow the user to put their particle onto a given space.

---
+++ resources/js/components/particle/ParticleForm.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="p-5">
+        <form @submit.prevent="onSubmit" class="space-y-3 mr-auto ml-auto">
+            <textarea v-model="body" rows="5" name="body" class="block border border-black w-3/4 p-2.5"></textarea>
+            <button type="submit" class="p-3 w-20 bg-indigo-300 rounded-lg">Post</button>
+        </form>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: ['space'],
+    data() {
+        return {
+            body: ''
+        }
+    },
+    methods: {
+        onSubmit() {
+            axios.post("/spaces/" + this.space +  "/particles", { body: this.body })
+        }
+    }
+}
+</script>
