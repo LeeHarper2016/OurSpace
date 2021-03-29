e7af42dc1e05defcc43ee3dd6feab1097f7c2c26
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-29 08:25:42 -0500

    Update app.js
    Updated: app.js
    -> Implemented the ErrorList class.
    -> Implemented functionality for an app-wide list of errors. The errorList will be injected into necessary components, and they will handle the addition of new errors.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,24 @@
 require('./bootstrap');
 
 import Vue from 'vue';
+import ErrorList from "./classes/ErrorList";
 import ErrorMessageList from "./components/error/ErrorMessageList";
 
 new Vue({
     el: '#app',
+    data: {
+        errorList: null
+    },
+    provide: [this.errorList],
     components: {
         ErrorMessageList
+    },
+    methods: {
+        addError(message) {
+            this.errorList.addError(message)
+        }
+    },
+    mounted() {
+        this.errorList = new ErrorList();
     }
 })
