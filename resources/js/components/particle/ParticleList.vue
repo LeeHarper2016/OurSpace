c69aec72fa6fee08fed66d6f190ccad6c9291167
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-29 11:36:39 -0500

    Create ParticleList.vue
    Created: ParticleList.vue
    -> Created a Vue component that will handle the display of all particles for a space.

---
+++ resources/js/components/particle/ParticleList.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="relative pointer-events-none w-full">
+        <div v-for="particle in this.particles">
+            <h1>{{ particle.user.name }}</h1>
+            <p>{{ particle.body }}</p>
+            <hr />
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: ['particles']
+}
+</script>
