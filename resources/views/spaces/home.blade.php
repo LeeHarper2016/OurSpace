681d5fb4c9ea5909a9ef28364bc63ac0778071c1
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-29 11:38:40 -0500

    Update home.blade.php
    Updated: home.blade.php
    -> Replaces blade implementation with new Vue component.

--- resources/views/spaces/home.blade.php
+++ resources/views/spaces/home.blade.php
@@ -6,11 +6,7 @@
         @if (count($space->particles) < 1)
             <span>There appears to be no particles currently posted to this space.</span>
         @else
-            @foreach($space->particles as $particle)
-                <h1>{{ $particle->user->name }}</h1>
-                <p>{{ $particle->body }}</p>
-                <hr />
-            @endforeach
+            <particle-list particles="@json($space->particles)"></particle-list>
         @endif
     </div>
 @endsection
