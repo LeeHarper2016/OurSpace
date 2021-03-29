7ce93e2c76bac64227dbe1d389a24b3400d05eb2
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-29 12:06:19 -0500

    Update home.blade.php
    Updated: home.blade.php
    -> Refactored the syntax to account for the new $particles parameter.
    -> Fixed an error where the particles attribute was not being fully recognized by vue.

--- resources/views/spaces/home.blade.php
+++ resources/views/spaces/home.blade.php
@@ -3,10 +3,10 @@
     @include('errors.summary')
     @include('particles.create')
     <div>
-        @if (count($space->particles) < 1)
+        @if (count($particles) < 1)
             <span>There appears to be no particles currently posted to this space.</span>
         @else
-            <particle-list particles='@json($space->particles)'></particle-list>
+            <particle-list :particles='@json($particles)'></particle-list>
         @endif
     </div>
 @endsection
