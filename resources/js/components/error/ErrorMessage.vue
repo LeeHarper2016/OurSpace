d1d865328d2d536cf899070325de285248414cca
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-29 16:07:14 -0500

    Update ErrorMessage.vue
    Updated: ErrorMessage.vue
    -> Added transition effects to the error message.
    -> Implemented component styles.

--- resources/js/components/error/ErrorMessage.vue
+++ resources/js/components/error/ErrorMessage.vue
@@ -1,10 +1,12 @@
 <template>
-    <div v-if="isVisible" class="bg-red-200 h-12 rounded-xl w-2/3 p-3 mr-auto ml-auto mt-3 pointer-events-auto">
-        <slot></slot>
-        <button @click="toggleVisibility" class="underline float-right">
-            close
-        </button>
-    </div>
+    <transition name="fade">
+        <div id="test" v-if="isVisible" class="bg-red-200 h-12 rounded-xl w-2/3 p-3 mr-auto ml-auto mt-3 pointer-events-auto">
+            <slot></slot>
+            <button @click="toggleVisibility" class="underline float-right">
+                close
+            </button>
+        </div>
+    </transition>
 </template>
 
 <script>
@@ -22,3 +24,14 @@
 
   }
 </script>
+
+<style scoped>
+
+.fade-enter-active, .fade-leave-active {
+    transition: opacity 0.75s;
+}
+.fade-enter, .fade-leave-to {
+    opacity: 0;
+}
+
+</style>
