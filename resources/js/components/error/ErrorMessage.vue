3d432c484cdac10bfa2d5f695fdc9e59e8abadb6
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-29 08:55:02 -0500

    Update ErrorMessage.vue

--- resources/js/components/error/ErrorMessage.vue
+++ resources/js/components/error/ErrorMessage.vue
@@ -1,7 +1,7 @@
 <template>
     <div v-if="isVisible" class="bg-red-200 h-12 rounded-xl w-2/3 p-3 mr-auto ml-auto mt-3 pointer-events-auto">
         <slot></slot>
-        <button @click="toggleVisibility" class="underline">
+        <button @click="toggleVisibility" class="underline float-right">
             close
         </button>
     </div>
