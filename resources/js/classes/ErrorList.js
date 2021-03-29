42cb3be80d4e9a12d4856bde362de7f367b11f20
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-03-29 08:36:17 -0500

    Update ErrorList.js
    Updated: ErrorList.js
    -> Changed instances of #errors to simply be "errors". This was due to private fields still being an experimental feature.

--- resources/js/classes/ErrorList.js
+++ resources/js/classes/ErrorList.js
@@ -1,46 +1,43 @@
 export default class ErrorList {
-    // Private field that will hold array of error messages.
-    #errors;
-
     /***********************************************************************
      *
      * Function Name: ErrorList()
      * Purpose: Acts as a default constructor for the ErrorList class. This
-     *          will instantiate the private #errors field with an empty
+     *          will instantiate the errors field with an empty
      *          array.
      * Precondition: N/A.
      * Postcondition: The new ErrorList object is initialized.
      *
      **********************************************************************/
     constructor() {
-        this.#errors = [];
+        this.errors = [];
     }
 
     /**********************************************************************
      *
      * Function Name: ErrorList.getErrors().
-     * Purpose: Returns the #errors value.
+     * Purpose: Returns the errors array value.
      * Precondition: N/A.
      * PostCondition: N/A.
      *
      *********************************************************************/
     getErrors() {
-        return this.#errors;
+        return this.errors;
     }
 
     /**********************************************************************
      *
      * Function Name: ErrorList.size().
      * Purpose: Returns the number of error messages currently residing in
-     *          the #errors field.
+     *          the errors field.
      * Precondition: N/A.
      * PostCondition: N/A.
      *
-     * @returns The amount of errors located within the #errors field.
+     * @returns The amount of errors located within the errors field.
      *
      *********************************************************************/
     size() {
-        return this.#errors.length;
+        return this.errors.length;
     }
 
     /**********************************************************************
@@ -48,11 +45,11 @@ export default class ErrorList {
      * Function Name: ErrorList.addError().
      * Purpose: Adds a new error message to the array of error messages.
      * Precondition: N/A.
-     * Postcondition: A new error message is added to the #errors array.
+     * Postcondition: A new error message is added to the errors array.
      *
      ********************************************************************/
     addError(message) {
-        this.#errors.append({
+        this.errors.append({
             id: this.size(),
             message: message
         });
@@ -61,13 +58,13 @@ export default class ErrorList {
     /*********************************************************************
      *
      * Function Name: ErrorList.deleteError(message).
-     * Purpose: Removes an error from the #errors array if it exists.
-     * Precondition: The error message must exist within the #errors array.
+     * Purpose: Removes an error from the errors array if it exists.
+     * Precondition: The error message must exist within the errors array.
      * Postcondition: The error message is removed from the array.
      *
      ********************************************************************/
     deleteError(message) {
-        this.#errors = this.#errors.filter((error) => error != message);
+        this.errors = this.errors.filter((error) => error != message);
     }
 
 }
