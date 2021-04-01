ccc395019c7b781ac557b82c9a47a5c144f2434c
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-04-01 11:05:50 -0500

    Create SpaceMenu.vue
    Created: SpaceMenu.vue
    -> This vue component will be how the user can search through all the spaces they are in.

---
+++ resources/js/components/space/SpaceMenu.vue
@@ -0,0 +1,34 @@
+<template>
+    <input type="text"
+           v-model="userSearch"
+           placeholder="Search Spaces"
+           class="text-black w-64 border p-1 pl-3 rounded-md" />
+    <ul>
+        <li v-for="space in searchList"
+            v-text="space.name">
+        </li>
+    </ul>
+</template>
+
+<script>
+
+export default {
+    props: ['spaces'],
+    data: {
+        userIsSearching: false,
+        userSearch: ''
+    },
+    computed: {
+        searchList() {
+            return this.spaces.filter((space) => {
+                return 'testing';
+            });
+        }
+    },
+    methods: {
+        removeError(id) {
+            this.errorList.deleteError(id);
+        }
+    },
+}
+</script>
