055f62e0140dca8d9a9ecfb59e7666d60c390eba
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-04-03 16:03:59 -0500

    Create NavbarDropdownMenu.vue
    Created: NavbarDropdownMenu.vue
    -> This Vue component will be responsible for handling the display of a dropdown menu for the navbar.

---
+++ resources/js/components/navbar/NavbarDropdownMenu.vue
@@ -0,0 +1,43 @@
+<template>
+    <div @mouseover="toggleHover">
+        <span>
+            <slot name="nav-option"></slot>
+        </span>
+        <transition name="slide">
+            <ul class="block text-black text-left absolute w-64 space-y-2 bg-white rounded-md p-2 border border-t-none"
+                v-if="userHoveredOverMenu">
+                <li v-for="item in items">
+                    <a :href="item.link" v-text="item.name"></a>
+                </li>
+            </ul>
+        </transition>
+    </div>
+</template>
+
+<style scoped>
+.slide-enter-active, .slide-leave-active {
+    transition: all 0.25s ease;
+}
+.slide-enter, .slide-leave-to {
+    transform: translateY(-1rem);
+    opacity: 0;
+}
+
+</style>
+
+<script>
+export default {
+    props: ['items'],
+    data() {
+        return {
+            userHoveredOverMenu: false
+        }
+    },
+    methods: {
+        toggleHover() {
+            this.userHoveredOverMenu = !this.toggleHover();
+        }
+    }
+}
+
+</script>
