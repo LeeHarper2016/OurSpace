74feec0265b8c80f52d07b176a728df27cb76f31
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-04-04 15:36:19 -0500

    Update navigation.blade.php
    Updated: navigation.blade.php
    -> Implemented the new navbar-menu component

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,10 +1,10 @@
-<div>
+<navbar-menu>
     @auth
-        <div class="flex justify-between w-full text-center text-white font-bold p-5">
-            <div class="space-x-5">
+        <navbar-menu>
+            <template #leftSide>
                 <a href="{{ url('/') }}" class="text-black font-bold">{{ env('APP_NAME') }}</a>
-            </div>
-            <div class="flex space-x-5">
+            </template>
+            <template #rightSide>
                 <space-menu :spaces='@json(auth()->user()->spaces)'></space-menu>
                 @if(isset($space))
                     <navbar-dropdown-menu :items='[{name: "Edit Space", link: "/spaces/{{ $space->id }}/edit"}, {name: "Delete Space", link: "/spaces/{{ $space->id }}/delete"}, ]'>
@@ -14,19 +14,21 @@
                 <navbar-dropdown-menu :items='[{name: "Profile", link: "/users/{{ auth()->user()->id }}"}, {name: "Edit Account", link: "/users/{{ auth()->user()->id }}/edit"}, ]'>
                     <template #nav-option>{{ auth()->user()->name }}</template>
                 </navbar-dropdown-menu>
-            </div>
-        </div>
+            </template>
+        </navbar-menu>
     @endauth
     @guest
-        <div class="float-right space-x-5 w-1/2 text-center text-white font-bold mr-auto ml-auto mt-5">
-            <a href="{{ url('/register') }}" class="p-3 bg-purple-700 hover:bg-purple-600 duration-200 border-2
+        <navbar-menu>
+            <template #rightSide>
+                <a href="{{ url('/register') }}" class="p-3 bg-purple-700 hover:bg-purple-600 duration-200 border-2
                 border-black rounded-md">
-                Register
-            </a>
-            <a href="{{ url('/login') }}" class="p-3 bg-purple-700 hover:bg-purple-600 duration-200 border-2
+                    Register
+                </a>
+                <a href="{{ url('/login') }}" class="p-3 bg-purple-700 hover:bg-purple-600 duration-200 border-2
                 border-black rounded-md">
-                Log In
-            </a>
-        </div>
+                    Log In
+                </a>
+            </template>
+        </navbar-menu>
     @endguest
 </div>
