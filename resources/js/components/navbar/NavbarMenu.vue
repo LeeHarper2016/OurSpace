f0aa14d048080589e847495f5cbac72c9437a923
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-04-04 15:32:17 -0500

    Update NavbarMenu.vue
    Updated: NavbarMenu.vue
    -> Implemented the first iteration of the template for the Navbar menu. It will have slots for the left and right-side menus.

--- resources/js/components/navbar/NavbarMenu.vue
+++ resources/js/components/navbar/NavbarMenu.vue
@@ -1,5 +1,12 @@
 <template>
-
+    <div class="flex justify-between w-full text-center text-white font-bold p-5">
+        <div class="space-x-5">
+            <slot name="leftMenu"></slot>
+        </div>
+        <div class="flex space-x-5">
+            <slot name="rightMenu"></slot>
+        </div>
+    </div>
 </template>
 
 <style scoped>
