c449301a221e401c1a0a8b28852835de3378d19e
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-04-05 13:30:47 -0500

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,7 +55,6 @@ class User extends Authenticatable
      *******************************************************************/
     public function spaces() {
         return $this->belongsToMany(Space::class, 'user_in_spaces')
-            ->withPivot('is_owner')
             ->withTimestamps();
     }
 }
