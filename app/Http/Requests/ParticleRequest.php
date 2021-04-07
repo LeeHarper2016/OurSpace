50bea740f2afb4440900cf72a023ab2e88347bdf
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-04-07 13:13:35 -0500

    Create ParticleRequest.php
    Created: ParticleRequest.php
    -> This request class will handle validation of all requests pertaining to particles.

---
+++ app/Http/Requests/ParticleRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ParticleRequest extends FormRequest
+{
+    /*********************************************************************
+     *
+     * Function Name: ParticleRequest.authorize().
+     * Purpose: Determine if the user is authorized to make this request.
+     *
+     * @return bool A boolean determining if the user is authorized.
+     *
+     ********************************************************************/
+    public function authorize() {
+        return false;
+    }
+
+    /*********************************************************************
+     *
+     * Function Name: ParticleRequest.rules().
+     * Purpose: The validation rules that the request must follow.
+     *
+     * @return array An array of rules for the request to accommodate.
+     *
+     ********************************************************************/
+    public function rules() {
+        return [
+            //
+        ];
+    }
+}
