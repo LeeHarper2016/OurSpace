9f7a36cb2df6bf6bf868c0276d3f37bca4a7b1bc
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-04-08 11:58:35 -0500

    Update ParticleTest.php
    Updated: ParticleTest.php
    -> Implemented a test for if a user that is in the designated space can post to it.
    -> Implemented a test for if a user that is not in the designated space can post to it.

--- tests/Unit/ParticleTest.php
+++ tests/Unit/ParticleTest.php
@@ -2,18 +2,44 @@
 
 namespace Tests\Unit;
 
+use App\Models\User;
 use Tests\TestCase;
 
 class ParticleTest extends TestCase
 {
-    /**
-     * A basic unit test example.
+    /*******************************************************************
      *
-     * @return void
-     */
-    public function test_a_basic_request() {
-        $response = $this->get('/');
+     * Test: user post particle success.
+     * Purpose: Tests if a user that has joined the space can post a
+     *          particle.
+     *
+     * TODO: Implement factories for self-contained testing.
+     *
+     ******************************************************************/
+    public function test_user_post_particle_success() {
+        $user = User::find(1);
 
-        $response->assertStatus(200);
+        $response = $this->actingAs($user)
+            ->post('/spaces/1/particles', ['body' => 'test']);
+
+        $response->assertStatus(201);
+    }
+
+    /*******************************************************************
+     *
+     * Test: user post particle fail.
+     * Purpose: Tests if a user that has not joined the space can post a
+     *          particle.
+     *
+     * TODO: Implement factories for self-contained testing.
+     *
+     ******************************************************************/
+    public function test_user_post_particle_failure() {
+        $user = User::find(3);
+
+        $response = $this->actingAs($user)
+            ->post('/spaces/1/particles', ['body' => 'test']);
+
+        $response->assertStatus(201);
     }
 }
