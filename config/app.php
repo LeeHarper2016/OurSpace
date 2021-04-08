68bd58672c2f0025a910e3efcc5c1eea83700420
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-04-08 11:37:03 -0500

    Update app.php
    Updated: app.php
    -> Included the FormRequestProvider as a new Service Provider.

--- config/app.php
+++ config/app.php
@@ -173,6 +173,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\FormRequestProvider::class,
         App\Providers\RouteServiceProvider::class,
 
     ],
