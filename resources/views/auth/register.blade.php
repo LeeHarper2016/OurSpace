10fd835c09df574c9eb764b7d05ea7299dc104fc
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-04-10 19:58:22 -0500

    Update register.blade.php
    Updated: register.blade.php
    -> Added an avatar image field to the registration form.
    -> Changed the enctype on the form to be multipart/form-data, so that the server could interpret the uploaded file.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,8 @@
 <div class="flex flex-col items-center mt-10">
     <img width="200" height="200" />
     <h1 class="text-4xl font-bold">Register</h1>
-    <form method="post" action="{{ url('/users')  }}" class="flex flex-col mr-auto ml-auto space-y-5 mt-5">
+    <form method="post" action="{{ url('/users')  }}" class="flex flex-col mr-auto ml-auto space-y-5 mt-5"
+          enctype="multipart/form-data">
         @csrf
         <div class="flex flex-col w-64">
             <label for="name">Name:</label>
@@ -12,6 +13,10 @@
         <div class="flex flex-col">
             <label for="email">Email:</label>
             <input type="email" name="email" value="{{ old('email') }}" />
+        </div>
+        <div class="flex flex-col">
+            <label for="avatar">Avatar Image:</label>
+            <input type="file" name="avatar" />
         </div>
         <div class="flex flex-col">
             <label for="password">Password:</label>
