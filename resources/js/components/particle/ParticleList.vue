6775943215ec685e877a94ec090ddb04ec67f926
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-04-10 21:12:14 -0500

    Update ParticleList.vue
    Updated: ParticleList.vue
    -> Implemented use of the ParticleSingle Vue component.

--- resources/js/components/particle/ParticleList.vue
+++ resources/js/components/particle/ParticleList.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="relative pointer-events-none w-full">
-        <div v-for="particle in this.particles" :key="particle.id">
-            <h1>{{ particle.user.name }}</h1>
-            <p>{{ particle.body }}</p>
-            <hr />
-        </div>
+        <ParticleSingle v-for="particle in this.particles" :particle="particle" :key="particle.id">
+            {{ particle.body }}
+        </ParticleSingle>
     </div>
 </template>
 
 <script>
 
+import ParticleSingle from "./ParticleSingle";
 export default {
+    components: {ParticleSingle},
     props: ['particles']
 }
 </script>
