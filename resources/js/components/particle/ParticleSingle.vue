2ffb7c960af5a4e9ef2f33b41494745a5557e8eb
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-04-10 21:11:37 -0500

    Create ParticleSingle.vue
    Created: ParticleSingle.vue
    -> This Vue component will be responsible for displaying a single particle in a space/user's timeline.

---
+++ resources/js/components/particle/ParticleSingle.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div class="relative pointer-events-none w-full">
+            <div class="p-5">
+                <img :src="'/' + particle.user.avatar" class="w-20 h-20 rounded-full inline" />
+                <h1 class="text-2xl font-bold inline">{{ particle.user.name }}</h1>
+                <br />
+                <p class="inline">
+                    <slot></slot>
+                </p>
+                <small class="block mt-3">Created at {{ Date(particle.created_at) }}</small>
+                <hr />
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    props: ['particle']
+}
+</script>
